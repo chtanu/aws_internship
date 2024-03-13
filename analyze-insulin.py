504e97215cafb702d5359dee7e019a741fa80b86
chtanu <149460824+chtanu@users.noreply.github.com>
2024-03-13 11:57:05 +0530

    Add files via upload

---
+++ analyze-insulin.py
@@ -0,0 +1,5 @@
+"""
+Your module description
+"""
+my string=" malwmrllpl lallalwgpd paaafvnqhl cgshlvealy lvcgergffy tpktrreaed
+        lqvgqvelgg gpgagslqpl alegslqkrg iveqcctsic slyqlenycn"
---
+++ numeric-data.py
@@ -0,0 +1,28 @@
+"""
+Your module description
+"""
+
+2+2
+4/2
+print("Python has three numeric types: int, float, and complex")
+python3 <lab-python-file-name>.py
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
